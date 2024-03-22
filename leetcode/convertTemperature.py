7f97222ccae3614b922fa2c1b0e7ef3ee0ccf173
Liben <152218324+Lib1221@users.noreply.github.com>
2024-03-23 00:41:23 +0300

    Create convertTemperature.py

---
+++ leetcode/convertTemperature.py
@@ -0,0 +1,8 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        
+        Kelvin = celsius + 273.15
+        Fahrenheit = celsius * 1.80 + 32.0
+        list = (Kelvin,Fahrenheit)
+        return list
+        
