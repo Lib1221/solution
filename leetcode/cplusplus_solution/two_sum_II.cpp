c0873351346a54061c91af5f6ed5a51f638a633a
Liben <152218324+Lib1221@users.noreply.github.com>
2024-03-23 20:20:46 +0300

    Create two_sum_II.cpp

---
+++ leetcode/cplusplus_solution/two_sum_II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+       int x=numbers.size();
+        int low=0;
+        int high = x-1;
+        while(low<=high){
+            if(numbers[low]+numbers[high]==target){
+                return {low+1,high+1};
+            }
+            else if(numbers[low]+numbers[high]>target){
+                high--;
+            }
+            else{
+                low++;
+            }
+        }
+        return {-1,-1};}
+};
