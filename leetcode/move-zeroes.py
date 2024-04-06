5954fbf702de9c5a46be53c080fcb575ab1bd83e
Liben <152218324+Lib1221@users.noreply.github.com>
2024-04-06 17:04:16 +0300

    Create move-zeroes.py

---
+++ leetcode/move-zeroes.py
@@ -0,0 +1,10 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        left = 0
+        right = 0
+        while right < len(nums):
+            if nums[right] != 0:
+                nums[right], nums[left] = nums[left], nums[right]
+                left +=1
+            right +=1
+        return nums
